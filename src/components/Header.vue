b0c20a4ccdfaf1da6fa1092faf2bc2420d0091fa
MaksimDyumin <maxdyumin@yandex.ru>
2023-03-22 20:30:17 +0300

    switch variable to production mode

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,7 +18,7 @@ export default {
   data: () =>{
     return {
       file: {},
-      domain: dev
+      domain: production
     }
   },
   methods: {
--- src/components/SheetApps.vue
+++ src/components/SheetApps.vue
@@ -53,12 +53,10 @@ const production = 'http://' + window.location.hostname
 const dev = 'http://127.0.0.1:5000'
 
 export default {
-
   data: () => {
     return {
-      domain: dev,
+      domain: production,
       dialog: false,
-      cameraNames: [],
       cameraToDelete: {}
     }
   },
