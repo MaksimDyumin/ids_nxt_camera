91be9cd1f38fcdf6cc7f1bdb61b63f8604eb197f
alexey.pavlov <pavlov_7@yahoo.de>
2023-03-25 14:19:59 +0100

    configure production build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/vapps/appman/website/' : '/',
+  productionSourceMap: process.env.NODE_ENV != 'production',
   transpileDependencies: [
     'vuetify'
   ]
